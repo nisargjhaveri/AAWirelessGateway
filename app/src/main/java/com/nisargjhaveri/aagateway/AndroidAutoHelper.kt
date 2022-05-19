101f4cd8508393cd5a9f3dfd1212882a6cfb45a9
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2022-05-19 14:23:47 +0530

    Add fallback package and activity for usb android auto on older devices

--- app/src/main/java/com/nisargjhaveri/aagateway/AndroidAutoHelper.kt
+++ app/src/main/java/com/nisargjhaveri/aagateway/AndroidAutoHelper.kt
@@ -3,6 +3,7 @@ package com.nisargjhaveri.aagateway
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
 import android.hardware.usb.UsbAccessory
 import android.hardware.usb.UsbManager
@@ -12,28 +13,60 @@ import org.lsposed.hiddenapibypass.HiddenApiBypass
 import java.lang.reflect.Method
 
 object AndroidAutoHelper {
-    private const val PACKAGE_NAME_ANDROID_AUTO_WIRELESS = "com.google.android.projection.gearhead"
-    private const val CLASS_NAME_ANDROID_AUTO_WIRELESS = "com.google.android.apps.auto.carservice.gmscorecompat.FirstActivityImpl"
     private const val ACTION_START_USB_ACCESSORY = "android.hardware.usb.action.USB_ACCESSORY_ATTACHED"
+
+    private const val PACKAGE_NAME_PROJECTION_GEARHEAD = "com.google.android.projection.gearhead"
+    private const val CLASS_NAME_CARSERVICE_FIRSTACTIVITY = "com.google.android.apps.auto.carservice.gmscorecompat.FirstActivityImpl"
+
+    private const val PACKAGE_NAME_ANDROID_GMS = "com.google.android.gms"
+    private const val CLASS_NAME_GMS_CAR_FIRSTACTIVITY = "com.google.android.gms.car.FirstActivity"
 
     // This requires that this app is a system app, and hidden api access is enabled
     fun startUsbAndroidAuto(context: Context, accessory: UsbAccessory): Boolean {
-        // Try to grant accessory permission to Android Auto
-        val uid = context.packageManager.getPackageUid(PACKAGE_NAME_ANDROID_AUTO_WIRELESS, PackageManager.GET_META_DATA)
-        if (!grantUsbAccessoryPermission(accessory, uid)) {
-            return false
+        getUsbAndroidAutoIntent(context, accessory)?.let { intent ->
+            val uid = context.packageManager.getPackageUid(intent.`package`!!, PackageManager.GET_META_DATA)
+            if (!grantUsbAccessoryPermission(accessory, uid)) {
+                return false
+            }
+
+            // Start Android Auto
+            context.startActivity(intent)
+            return true
         }
 
-        // Start Android Auto
-        val intent = Intent()
+        return false
+    }
+
+    private fun getUsbAndroidAutoIntent(context: Context, accessory: UsbAccessory): Intent? {
+        Intent()
             .setAction(ACTION_START_USB_ACCESSORY)
-            .setClassName(PACKAGE_NAME_ANDROID_AUTO_WIRELESS, CLASS_NAME_ANDROID_AUTO_WIRELESS)
+            .setPackage(PACKAGE_NAME_PROJECTION_GEARHEAD)
+            .setClassName(PACKAGE_NAME_PROJECTION_GEARHEAD, CLASS_NAME_CARSERVICE_FIRSTACTIVITY)
             .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             .putExtra(UsbManager.EXTRA_ACCESSORY, accessory)
+            .let { intent ->
+                intent.resolveActivityInfo(context.packageManager, 0)?.let {
+                    if (it.exported) {
+                        return intent
+                    }
+                }
+            }
 
-        context.startActivity(intent)
+        Intent()
+            .setAction(ACTION_START_USB_ACCESSORY)
+            .setPackage(PACKAGE_NAME_ANDROID_GMS)
+            .setClassName(PACKAGE_NAME_ANDROID_GMS, CLASS_NAME_GMS_CAR_FIRSTACTIVITY)
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            .putExtra(UsbManager.EXTRA_ACCESSORY, accessory)
+            .let { intent ->
+                intent.resolveActivityInfo(context.packageManager, 0)?.let {
+                    if (it.exported) {
+                        return intent
+                    }
+                }
+            }
 
-        return true
+        return null
     }
 
     @SuppressLint("SoonBlockedPrivateApi", "PrivateApi", "DiscouragedPrivateApi")
